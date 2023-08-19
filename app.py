ef513653b122f66d124cdb8fe9338477644f5235
Kauan Arthur <raidoka546@gmail.com>
2023-08-19 20:39:11 -0300

    Adição do código.

---
+++ app.py
@@ -0,0 +1,30 @@
+5455try:
+    num1 = int(input('Digite um numero: '))
+    num2 = int(input('Digite outro numero: '))
+    op = input('Digite um operador: ')
+
+    if op == '+':
+        print(num1 + num2)
+        
+    elif op == '-':
+        print(num1 - num2)
+        
+    elif op == '/':
+        print(num1 / num2)
+        
+    elif op == '*':
+        print(num1 * num2)
+
+    elif op == '**':
+        print(num1 ** num2)
+        
+    elif op == '%':
+        print(num1 % num2)
+        
+    elif op == '//':
+        print(num1 // num2)
+        
+    else:
+        print('Escolha um operador válido.')
+except:
+    print('Escolha um NÚMERO válido.')
